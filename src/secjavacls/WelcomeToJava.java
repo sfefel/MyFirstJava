3720ee61d4a6784e0c0c247689cc11d4819a3616
shirley fefel <sfefel.sf@gmail.com>
2018-04-24 19:05:43 -0500

    First projects to hub

---
+++ src/secjavacls/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package secjavacls;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("here is what is being printed");		 
+
+	}
+
+}
